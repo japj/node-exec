5f89daa8197b85036f05c16ce46bc16edbcc0959
Jeroen Janssen <jeroen.janssen@gmail.com>
2012-05-28 21:48:17 +0200

    test: log output of exec

--- test/test-exec-sm.js
+++ test/test-exec-sm.js
@@ -1,3 +1,9 @@
 var EXEC = require('../lib/exec').exec;
 
-EXEC("sm --version");
+EXEC("sm --version", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
